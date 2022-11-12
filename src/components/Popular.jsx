0086b2702744c99deb097d981df0de989c205729
Gustavo <gustavo.loureiro@buser.com.br>
2022-11-12 18:30:00 -0300

    @more stuff

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
+import styled from "styled-components";
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
 function Popular () {
@@ -12,28 +12,45 @@ function Popular () {
     }, []);
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`);
-        const data = await api.json();
-        setPopular(data.recipes);
+
+        const check = localStorage.getItem('popular');
+
+        if (check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`);
+            const data = await api.json();
+
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+        }
     }
 
     return (
         <div>
-            {popular.map((recipe) => {
-                return (
-                    <Wrapper key={recipe.id}>
-                        <h3>Popular Picks</h3>
-                        {popular.map((recipe) => {
-                            return (
+            <Wrapper>
+                <h3>Popular Picks</h3>
+                <Splide
+                    options={{
+                        perPage: 4,
+                        arrows: false,
+                        pagination: false,
+                        drag: 'free',
+                        gap: '5rem',
+                    }}
+                >
+                    {popular.map((recipe) => {
+                        return (
+                            <SplideSlide key={recipe.id}>
                                 <Card>
                                     <p>{recipe.title}</p>
                                     <img src={recipe.image} alt={recipe.title} />
                                 </Card>
-                            )
-                        })}
-                    </Wrapper>
-                );
-            })}
+                            </SplideSlide>
+                        )
+                    })}
+                </Splide>
+            </Wrapper>
         </div>
     )
 }
@@ -46,10 +63,31 @@ const Card = styled.div`
     min-width: 25rem;
     border-radius: 2rem;
     overflow: hidden;
+    position: relative;
 
     img {
-        width: 25%;
-        border-radius: 2rem;
+        border-radius: 1rem;
+        /* position: absolute; */
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 50%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
     }
 `;
 
