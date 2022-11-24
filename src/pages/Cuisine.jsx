90806cc7721f0630cc6823d0898ccbc3abc767ec
Gustavo <gustavo.loureiro@buser.com.br>
2022-11-24 18:04:36 -0300

    @looking nice, joker!

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -20,8 +20,35 @@ function Cuisine () {
     }, [params.type]);
 
     return (
-        <div></div>
+        <Grid>
+            {cuisine.map((item) => (
+                <Card key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Card>
+            ))}
+        </Grid>
     )
 }
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+    img {
+        width: 100%;
+        border-radius: 2rem;
+    }
+    a {
+        text-decoration: none;
+    }
+    h4 {
+        text-align: center;
+        padding: 1rem;
+    }
+`;
 
 export default Cuisine;
