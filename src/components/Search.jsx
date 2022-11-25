c830fa71c6ecabb0b4ed17ff7e3258b2d3b67c1e
Gustavo <gustavo.loureiro@buser.com.br>
2022-11-24 21:02:46 -0300

    @Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+function Search () {
+    return (
+        <form action="">
+            <input type="text" />
+        </form>
+    )
+}
+
+export default Search
